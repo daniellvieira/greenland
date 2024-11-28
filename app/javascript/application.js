9d3249e3ba22e28c930a34d02739bc3dd1770ed1
Daniel Vieira <danielvieiraufc@gmail.com>
2024-11-28 16:04:42 -0300

    feat: add data animate out when unfavorite a concert

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,18 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import "./components/venue_display"
 import "animate.css"
+
+// https://discuss.hotwired.dev/t/are-transitions-and-animations-on-hotwire-roadmap/1547
+document.addEventListener("turbo:before-stream-render", (event) => {
+  if (event.target.action === "remove") {
+    const targetFrame = document.getElementById(event.target.target)
+    if (targetFrame.dataset.animateOut) {
+      event.preventDefault()
+      const elementBeingAnimated = targetFrame
+      elementBeingAnimated.classList.add(targetFrame.dataset.animateOut)
+      elementBeingAnimated.addEventListener("animationend", () => {
+        targetFrame.remove()
+      })
+    }
+  }
+})
